b91eb952e6268e092827c84e3dd0a2059f1393b0
YukiOldeus <yuki.nguyen@oldeus.com>
2023-08-17 11:39:29 +0700

    update button style

--- src/containers/LandingAbout/LandingAbout.tsx
+++ src/containers/LandingAbout/LandingAbout.tsx
@@ -4,7 +4,7 @@ import { Contact, Faq } from "."
 function LandingAbout() {
   const [isMobileScreen] = useMediaQuery('(max-width: 768px)')
   return (
-    <Center className="w-full h-full bg-black relative items-start md:items-center pt-[68px]">
+    <Center className="w-full h-full bg-black relative items-start md:items-center pt-[80px]">
       <Container maxWidth={'1320px'} className="px-6 md:px-[120px] 2xl:px-24 static md:absolute" top={!isMobileScreen ? '200px' : '0px'}>
         <Flex className='flex flex-col mt-[15px] md:flex-row gap-x-4'>
           <Faq />
