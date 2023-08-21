434c98e875cafe78b8f3c9e53c025b78f38d16a9
dai nguyen <dangdainguyen.cs@gmail.com>
2023-08-22 06:54:01 +0700

    fix: adjustment faq

--- src/containers/LandingAbout/LandingAbout.tsx
+++ src/containers/LandingAbout/LandingAbout.tsx
@@ -4,9 +4,9 @@ import { Contact, Faq } from "."
 function LandingAbout() {
   const [isMobileScreen] = useMediaQuery('(max-width: 768px)')
   return (
-    <Center className="w-full h-full bg-black relative items-start md:items-center pt-[100px]">
+    <Center className="w-full h-full bg-black relative items-start md:items-center pt-[60px] md:pt-[100px]">
       <Container maxWidth={'1320px'} className="px-10 md:px-[120px] 2xl:px-24 static md:absolute" top={!isMobileScreen ? '150px' : '0px'}>
-        <Flex className='flex flex-col mt-[15px] md:flex-row gap-x-4'>
+        <Flex className='flex flex-col mt-0 md:mt-[15px] md:flex-row gap-x-4'>
           <Faq />
           {
             !isMobileScreen && <Contact />
