359ec25bc3421cfffd9db46f84bd70a836163820
YukiOldeus <yuki.nguyen@oldeus.com>
2023-09-29 14:09:38 +0700

    update feedback 29/9

--- src/containers/LandingAbout/LandingAbout.tsx
+++ src/containers/LandingAbout/LandingAbout.tsx
@@ -5,7 +5,7 @@ function LandingAbout() {
   const [isMobileScreen] = useMediaQuery('(max-width: 768px)')
   return (
     <Center className="w-full h-auto bg-black relative items-start md:items-center pt-[60px] md:pt-[100px]" bgImage={`url("/images/noise.png")`}>
-      <Container maxWidth={'940px'} className="px-10 md:px-[120px] 2xl:px-24 static md:absolute" top={!isMobileScreen ? '150px' : '0px'}>
+      <Container maxWidth={'940px'} className="px-10 md:px-[120px] 2xl:px-24 static md:absolute" top={!isMobileScreen ? '180px' : '0px'}>
         <Flex className='flex flex-col mt-0 md:mt-[15px] md:flex-row gap-x-4'>
           <Faq />
         </Flex>
