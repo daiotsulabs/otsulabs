613918c69eb1dcab5785ee6df309e567ed44ad2c
ngoc.pham3 <ngoc.pham3@sotatek.com>
2023-11-24 00:29:33 +0700

    update button change language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import { Box, Button, Stack, useMediaQuery } from "@chakra-ui/react";
 import { CancelIcon, MenuIcon, SingleLogo } from "../icons";
 import { useRouter } from "next/navigation";
 import { useScrollHeader } from "@/hooks/useScrollHeader";
+import { LanguageButton } from "../LanguageButton";
 
 interface HeaderProps {
   dark?: boolean;
@@ -25,7 +26,7 @@ function Header({
   toHome = true,
   dark = false,
   visibleHeader = false,
-  customClassName = ""
+  customClassName = "",
 }: HeaderProps) {
   const router = useRouter();
   const [isMobileScreen] = useMediaQuery("(max-width: 480px)");
@@ -34,11 +35,14 @@ function Header({
   return (
     <header
       className={`fixed pl-6 pr-[20px] md:top-[16px] md:inset-x-11 md:top-3 z-[19] h-[60px] w-full md:w-[initial] 
-      ${dark ? "bg-transparent" : "bg-black/30"} backdrop-blur-xl md:rounded-full transition-transform duration-700 ease-in-out transform 
-      ${isHeaderVisible || visibleHeader
+      ${
+        dark ? "bg-transparent" : "bg-black/30"
+      } backdrop-blur-xl md:rounded-full transition-transform duration-700 ease-in-out transform 
+      ${
+        isHeaderVisible || visibleHeader
           ? "-translate-y-[76px]"
           : "translate-y-0"
-        } ${customClassName} 
+      } ${customClassName} 
       `}
     >
       <Stack
@@ -83,23 +87,33 @@ function Header({
               </Button>
             ))}
         </Stack>
-        <Button
-          cursor="pointer"
-          onClick={onClickToggle}
-          p={0}
-          className="text-xs xl:text-md"
-          variant="unstyled"
-          sx={{ minW: 0 }}
-          color={dark ? "black" : "#f5f5f5"}
-          w={50}
-          textAlign={isMobileScreen ? "right" : "center"}
-        >
-          {
-            isMobileScreen ?
-            <MenuIcon className={`${ isCancelIcon ? 'rotate-45' : '' } duration-400 transition-[transform] ml-auto`} fill={dark ? "black" : "white"} />
-            : isCancelIcon ? 'CLOSE' : 'MENU'
-          }
-        </Button>
+        <Stack className="flex-row items-center">
+          <LanguageButton />
+          <Button
+            cursor="pointer"
+            onClick={onClickToggle}
+            p={0}
+            className="text-xs xl:text-md"
+            variant="unstyled"
+            sx={{ minW: 0 }}
+            color={dark ? "black" : "#f5f5f5"}
+            w={50}
+            textAlign={isMobileScreen ? "right" : "center"}
+          >
+            {isMobileScreen ? (
+              <MenuIcon
+                className={`${
+                  isCancelIcon ? "rotate-45" : ""
+                } duration-400 transition-[transform] ml-auto`}
+                fill={dark ? "black" : "white"}
+              />
+            ) : isCancelIcon ? (
+              "CLOSE"
+            ) : (
+              "MENU"
+            )}
+          </Button>
+        </Stack>
       </Stack>
     </header>
   );
